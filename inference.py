33d74cd620da6d85c4e2971d0b6d68b738295b83
qiwen-1123 <qiwen_xu@yahoo.com>
2023-12-04 18:13:45 +0100

    inference and save both overlay and masks; define class and mask jsons

--- inference.py
+++ inference.py
@@ -29,6 +29,14 @@ import warnings
 import cv2
 import numpy as np
 import tqdm
+from rich.progress import (
+    Progress,
+    TextColumn,
+    BarColumn,
+    TimeElapsedColumn,
+    TimeRemainingColumn,
+    track,
+)
 
 from detectron2.config import get_cfg
 from detectron2.data.detection_utils import read_image
@@ -88,50 +96,108 @@ def get_parser():
     return parser
 
 
-def save_masks(preds, text):
+def get_masks(preds, text):
+    mask_json_filepath = "./datasets/nuscenes_mask_dict.json"
+
+    with open(mask_json_filepath, "r") as json_file:
+        mask_dict = json.load(json_file)
+
+    mask_image = np.zeros(
+        (preds["sem_seg"].shape[1], preds["sem_seg"].shape[2], 3), dtype=np.uint8
+    )
     preds = preds["sem_seg"].argmax(dim=0).cpu().numpy()  # C H W
+
+    output_path = "output"
+    dir_mask_all = os.path.join(output_path, "_mask_all.jpg")
+
     for i, t in enumerate(text):
-        dir = f"mask_{t}.png"
+        dir = os.path.join(output_path, f"{t}_mask.jpg")
         mask = preds == i
-        cv2.imwrite(dir, mask * 255)
+        mask_image[mask] = mask_dict[t]
+
+    return mask_image
 
 
 def predict(image, text, model_type):
     use_sam = model_type != "CAT-Seg"
 
     predictions, visualized_output = demo.run_on_image(image, text, use_sam)
-    # save_masks(predictions, text.split(','))
+    # save_masks(predictions, text.split(","))
     canvas = fc(visualized_output.fig)
     canvas.draw()
     out = np.frombuffer(canvas.tostring_rgb(), dtype="uint8").reshape(
         canvas.get_width_height()[::-1] + (3,)
     )
 
-    return out[..., ::-1]
+    return out[..., ::-1], predictions
 
 
 if __name__ == "__main__":
     args = get_parser().parse_args()
     cfg = setup_cfg(args)
     global demo
-    
+
     demo = VisualizationDemo(cfg)
     image_path = "nuscenes.jpg"
     image = cv2.imread(image_path)
-    
-    json_file_path = 'datasets/nuscenes.json'
-    
-    with open(json_file_path, 'r') as file:
+
+    json_file_path = "datasets/nuscenes.json"
+
+    with open(json_file_path, "r") as file:
         data_list = json.load(file)
         result_string = ",".join(data_list)
-        
-    ouputs = predict(
-        image, result_string, model_type="CAT-Seg"
-    )
 
-    cv2.imshow("Image Visualization", ouputs)
+    input_path = "datasets/nuscenes"
+    output_path = "output"
 
-    key = cv2.waitKey(0)
-    if key != None:
-        cv2.destroyAllWindows() 
-        exit()  
+    with Progress(
+        TextColumn("[progress.description]{task.description}"),
+        BarColumn(),
+        TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
+        TimeRemainingColumn(),
+        TimeElapsedColumn(),
+    ) as progress:
+        task = progress.add_task(
+            "[cyan]Processing Images", total=len(os.listdir(input_path)), unit="image"
+        )
+
+        for filename in os.listdir(input_path):
+            if filename.endswith((".jpg", ".jpeg", ".png")):
+                image_path = os.path.join(input_path, filename)
+                image = cv2.imread(image_path)
+                image_name = filename[: filename.rfind(".")]
+
+                outputs, predictions = predict(
+                    image, result_string, model_type="CAT-Seg"
+                )
+                mask_image = get_masks(predictions, result_string.split(","))
+
+                output_overlay_image_path = os.path.join(
+                    output_path, f"{image_name}_overlay.jpg"
+                )
+                output_mask_image_path = os.path.join(
+                    output_path, f"{image_name}_mask.jpg"
+                )
+
+                cv2.imwrite(output_overlay_image_path, outputs)
+                cv2.imwrite(output_mask_image_path, mask_image)
+
+                progress.update(task, advance=1)
+
+    # for filename in track(os.listdir(input_path)):
+    #     if filename.endswith((".jpg", ".jpeg", ".png")):
+    #         image_path = os.path.join(input_path, filename)
+    #         image = cv2.imread(image_path)
+    #         outputs = predict(image, result_string, model_type="CAT-Seg")
+
+    #         output_image_path = os.path.join(output_path, f"{filename}_output.jpg")
+    #         cv2.imwrite(output_image_path, outputs)
+
+    # ouputs = predict(
+    #     image, result_string, model_type="CAT-Seg"
+    # )
+    # cv2.imshow("Image Visualization", ouputs)
+    # key = cv2.waitKey(0)
+    # if key != None:
+    #     cv2.destroyAllWindows()
+    #     exit()
